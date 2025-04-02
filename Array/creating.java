c3910e736edeb4172883e10f5288e12feb0b3a65
Nishan kumar Rai <infonkumarrai323@gmail.com>
2025-04-02 14:34:56 +0300

    Liner Search

--- Array/creating.java
+++ Array/creating.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 
 public class creating {
+
+
+    public static void update(int studentMark[]){
+        for(int i =0 ; i<studentMark.length; i++){
+            studentMark[i] =studentMark[i] + 1;
+        }
+    }
     public static void main(String[] args) {
-       String fruits[]={"apple", "mango", "banana"};
-       System.out.println(fruits.length);
-
-       int marks[]= new int[100];
-       Scanner sc = new Scanner(System.in);
-
-       marks[0]= sc.nextInt();
-       marks[1]=sc.nextInt();
-       System.out.println("phy: " + marks[0]);
+     
+       int studentMark[]={97,98,99};
+       update(studentMark);
+       // print our marks
+       for(int i =0; i<studentMark.length; i++){
+        System.out.println(studentMark[i] + "");
+       }    
     }
 }
---
+++ Array/problem.java
@@ -0,0 +1,26 @@
+
+
+public class problem {
+    public static void valueUpdate(int arr[], int factor){
+        for(int i =0; i<arr.length; i++){
+            arr[i] = arr[i] * factor;
+        }
+    }
+    public static void main(String[] args) {
+    // Problem 1: Array Scaling
+    // Create a method that takes an integer array and a scaling factor as input.
+    // The method should multiply each element of the array by the scaling factor.
+    // Example:
+    // Input: array = {1, 2, 3}, factor = 2
+    // Output: array = {2, 4, 6}
+    int arr[]={1,2,3};
+    int factor=2;
+    valueUpdate(arr, factor);
+
+    //printing 
+    for(int i =0;i<arr.length; i++){
+        System.out.println(arr[i]);
+    }
+    
+    }
+}
